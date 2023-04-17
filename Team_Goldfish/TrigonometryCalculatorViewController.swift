ceff376b572d2cf0056cd280c6010eb91a6bd427
TateArmstrong <yourdawgphil@gmail.com>
2023-04-16 20:59:11 -0500

    Added formula label for trigonometric calculator with functionality to add and remove characters from it. 🧮

--- Team_Goldfish/TrigonometryCalculatorViewController.swift
+++ Team_Goldfish/TrigonometryCalculatorViewController.swift
@@ -9,13 +9,59 @@ import UIKit
 
 class TrigonometryCalculatorViewController: UIViewController {
 
+    @IBOutlet weak var formulaOutput: UILabel!
+    @IBOutlet weak var resultOutput: UILabel!
+    
+    var formulaStr = "0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Initialize the output labels to 0.
+        formulaOutput.text = formulaStr
+        resultOutput.text = "0"
     }
     
-
+    // Number/Symbol button functionality.
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "no title")
+        // If the formula text shows 0, replace the text,
+        // else append the text
+        if(formulaStr == "0"){
+            formulaStr = sender.titleLabel?.text ?? "no title"
+        }
+        else {
+            formulaStr += sender.titleLabel?.text ?? "no title"
+        }
+        
+        // Set the output text to the formulaStr
+        formulaOutput.text = formulaStr
+    }
+    
+    // Backspace button functionality.
+    @IBAction func backTapped(_ sender: Any) {
+        // If the formula has more that 1 char, remove the last char.
+        // Else assign it 0.
+        if(formulaStr.count > 1){
+            formulaStr.removeLast()
+            formulaOutput.text = formulaStr
+        }
+        else {
+            formulaStr = "0"
+            formulaOutput.text = formulaStr
+        }
+    }
+    
+    // Clear the result and formula label.
+    @IBAction func clearTapped(_ sender: Any) {
+        formulaStr = "0"
+        formulaOutput.text = "0"
+        resultOutput.text = "0"
+    }
+    
+    // Evaluate the formula
+    @IBAction func evaluateTapped(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
